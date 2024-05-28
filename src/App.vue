ed600c859bbe92d25ecdba78e86d359a90b71e72
Olena I <ilchenko.h.v@gmail.com>
2024-05-28 22:51:23 +0200

    adding header + footer components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
-  <h1>Hello World!</h1>
+  <div class="wrapper">
+    <TheHeader />
+    <div class="wrapper-content">
+      <div class="container">
+        <h1>Hello World!</h1>
+      </div>
+    </div>
+    <TheFooter />
+  </div>
 </template>
 
 <script>
+import TheHeader from '@/components/TheHeader';
+import TheFooter from '@/components/TheFooter';
 
-export default {};
+export default {
+  components: {
+    TheHeader, 
+    TheFooter
+  },
+};
 </script>
 
 <style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="container">
+        <p>Header</p>
+    </div>
+</template>
