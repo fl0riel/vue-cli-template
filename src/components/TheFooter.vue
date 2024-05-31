1555e0ec6cae0862bacee3707803bf42c065ef7c
Olena I <ilchenko.h.v@gmail.com>
2024-05-31 22:10:30 +0200

    adding two types of menu in components

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,14 @@
 <template>
-    <div class="container">
-        <p>Footer</p>
-    </div>
+    <footer class="footer">
+        <div class="container">
+            <ul class="navbar-list">
+                <li class="navbar-item">
+                    <a href="/" class="navbar-link">Home</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="/about" class="navbar-link">About</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
 </template>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,35 @@
 <template>
-    <div class="container">
-        <p>Header</p>
-    </div>
+    <header class="navbar">
+        <div class="container">
+            <div class="navbar-content">
+                <a class="navbar-logo" href="/">Logo</a>
+                <ul class="navbar-list">
+                    <li class="navbar-item" v-for="link in links" :key="link.alias">
+                        <a :href="link.url" class="navbar-link">{{ link.title }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    title: 'Home',
+                    alias: 'home',
+                    url: '/',
+                },
+                {
+                    title: 'About',
+                    alias: 'about',
+                    url: '/about',
+                },
+            ],
+        };
+    },
+};
+</script>
